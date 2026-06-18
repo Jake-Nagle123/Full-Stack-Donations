37478dc8739d492168e4a4d9e04754425adc8615
Jake Nagle <jake.nagle54@gmail.com>
2026-06-18 16:39:46 +0100

    DOM Access added.

--- todo.js
+++ todo.js
@@ -3,5 +3,11 @@ let count = 0;
 
 function addTodo() {
   count++;
-  console.log(`Add todo call ${count}`) // JavaScript Template Strings ie ``
+  const todoText = document.getElementById("id-todo").value;
+  console.log(`Add todo call ${count}: ${todoText}`) // JavaScript Template Strings ie ``
+
+  const table = document.getElementById("todo-table");
+  const row = table.insertRow(-1);
+  const textCell = row.insertCell(0);
+  textCell.innerText = todoText;
 }
