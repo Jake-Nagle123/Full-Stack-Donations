4d8ebf357d1e1785c1c65333f521726d6dbe310e
Jake Nagle <jake.nagle54@gmail.com>
2026-06-18 19:57:29 +0100

    Refactor + Debugged

--- todo.js
+++ todo.js
@@ -1,13 +1,18 @@
 let todoItems = [];
-let count = 0;
 
-function addTodo() {
-  count++;
-  const todoText = document.getElementById("id-todo").value;
-  console.log(`Add todo call ${count}: ${todoText}`) // JavaScript Template Strings ie ``
 
+function renderTodo(todo) {
   const table = document.getElementById("todo-table");
   const row = table.insertRow(-1);
   const textCell = row.insertCell(0);
   textCell.innerText = todoText;
+}
+
+function addTodo() {
+  const todoText = document.getElementById("id-todo").value;
+  const todo = {
+    text: todoText
+  };
+  todoItems.push(todo);
+  renderTodo(todo);
 }
