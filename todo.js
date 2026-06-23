bea3aae672664d03f24dd46b6d16094e07ddaf9b
Jake Nagle <jake.nagle54@gmail.com>
2026-06-23 13:01:18 +0100

    More debugging and added delete Event Handler.

--- todo.js
+++ todo.js
@@ -11,7 +11,7 @@ function renderTodo(todo) {
   dateCell.innerText = todo.date;
 
   const deleteCell = row.insertCell(2);
-  deleteCell.innerHTML = `<a class="button">delete</a>`
+  deleteCell.innerHTML = `<a onClick="deleteTodo('${todo.id}')" class="button">delete</a>`;
 }
 
 function addTodo() {
@@ -30,4 +30,8 @@ function uuidv4() {
     var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
+}
+
+function deleteTodo(id) {
+  console.log(`Delete item ${id}`);
 }
