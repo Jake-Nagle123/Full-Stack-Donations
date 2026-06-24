ebdd8c6f58ef17b185627d14a70bcecbdf992195
Jake Nagle <jake.nagle54@gmail.com>
2026-06-24 20:06:41 +0100

    Todo Form, Styles added as well as Svelte project created.

--- todo-svelte/src/routes/+page.svelte
+++ todo-svelte/src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  let todoText;
+  function addTodo() {
+    console.log(todoText)
+  }
+</script>
+
+<div class="container">
+  <div class="box has-text-centered">
+    <div class="title">Simple Todo List</div>
+    <div class="subtitle">Fun things to do</div>
+  </div>
+</div>
+<div class="section box">
+  <div class="field is-horizontal">
+    <div class="field-label is-normal">
+      <label for="todo" class="label">What Should I do?</label>
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <p class="control">
+          <input bind:value={todoText} id="todo" class="input" type="text" placeholder="Type something...">
+        </p>
+      </div>
+      <button onclick={() => addTodo()} class="button">Add Todo</button>
+    </div>
+  </div>
+</div>
